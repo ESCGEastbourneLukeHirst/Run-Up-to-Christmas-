9c7c5f66d79925f9f8e058d03062a2ac0637e08c
Luke Hirst <521480@chrome.escg.ac.uk>
2022-12-09 16:25:37 +0000

    Updated Advent Calendar

---
+++ Assets/Scripts/ChristmasCountdown.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChristmasCountdown : MonoBehaviour
+{
+    Image img;
+
+    public Color Color { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Color = new Color(img.color.r, img.color.g, img.color.b, img.color.a);
+            print("On This Day, you received...");
+        }
+    }
+}
